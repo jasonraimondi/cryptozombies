ec6debacb85a72d3217ef33c8625b81ad821bdf8
Jason Raimondi <jason@raimondi.us>
2021-10-28 22:43:02 -0700

    feat: show zombie details

--- web/src/routes/index.svelte
+++ web/src/routes/index.svelte
@@ -3,22 +3,26 @@ import { browser } from '$app/env';
 
 import { store } from "$lib/store";
 import ConnectWallet from "$lib/components/ConnectWallet.svelte";
+import { BigNumber } from "ethers";
 
 $: isContractInitialized = browser && window.zombieOwnership === undefined;
 
-async function getZombies() {
+async function getZombies(): Promise<BigNumber[]> {
   return window.zombieOwnership.getZombiesByOwner($store.currentAddress);
 }
 
-async function getZombie(zombieId: string) {
-  return window.zombieOwnership.zombies(zombieId);
+async function getZombie(zombieId: string | BigNumber) {
+  zombieId = typeof zombieId === "string" ? zombieId : zombieId.toString();
+  const zombie = await window.zombieOwnership.zombies(zombieId);
+  return { id: zombieId, ...zombie };
 }
 
+let zombie;
+
 async function createZombie() {
-  await window.zombieOwnership.createRandomZombie("Ruby");
-  const zombies = await getZombies();
-  console.log({ zombies })
-  const zombie = await getZombie(zombies[0]);
+  await window.zombieOwnership.createRandomZombie("Ruby").catch(() => {});
+  const [id] = await getZombies();
+  zombie = await getZombie(id);
   console.log({ zombie })
 }
 </script>
@@ -27,10 +31,20 @@ async function createZombie() {
   <ConnectWallet />
 {:else}
   {#if isContractInitialized}
-    <button on:click={createZombie}>Create Zombie</button>
+    <button on:click={createZombie}>Fetch Zombie</button>
   {:else}
     Nothing is here
   {/if}
 {/if}
 
 {JSON.stringify($store)}
+
+{#if zombie}
+<div>
+  <p>id: {zombie.id}</p>
+  <p>dna: {zombie.dna}</p>
+  <p>name: {zombie.name}</p>
+  <p>winCount: {zombie.winCount}</p>
+  <p>lossCount: {zombie.lossCount}</p>
+</div>
+{/if}
